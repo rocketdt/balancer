1b484579f12fc943a2ac2f24fa926d1b851ef0f9
rocketdt <kingcchan@yahoo.com>
2024-05-14 12:29:21 +0800

    copy first balancer from base balancer

--- balancer/first.go
+++ balancer/first.go
@@ -4,20 +4,23 @@
 
 package balancer
 
+import (
+	"sync"
+)
+
 func init() {
 	factories[FirstBalancer] = NewFirst
 }
 
 type First struct {
-	BaseBalancer
+	sync.RWMutex
+	hosts []string
 }
 
 // NewRandom create new Random balancer
 func NewFirst(hosts []string) Balancer {
 	return &First{
-		BaseBalancer: BaseBalancer{
-			hosts: hosts,
-		},
+		hosts: hosts,
 	}
 }
 
@@ -31,6 +34,7 @@ func (r *First) Balance(_ string) (string, error) {
 	return r.hosts[0], nil
 }
 
+// Add new host to the balancer
 func (b *First) Add(host string) {
 	b.Lock()
 	defer b.Unlock()
@@ -41,3 +45,21 @@ func (b *First) Add(host string) {
 	}
 	b.hosts = append(b.hosts, host)
 }
+
+// Remove new host from the balancer
+func (b *First) Remove(host string) {
+	b.Lock()
+	defer b.Unlock()
+	for i, h := range b.hosts {
+		if h == host {
+			b.hosts = append(b.hosts[:i], b.hosts[i+1:]...)
+			return
+		}
+	}
+}
+
+// Inc .
+func (b *First) Inc(_ string) {}
+
+// Done .
+func (b *First) Done(_ string) {}
